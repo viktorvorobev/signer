6493d545b3f9896f1de595876eef929b01636d7b
Viktor Vorobev <vikeyer@gmail.com>
2024-11-02 16:14:28 +0100

    src: tests: pdf_creator: add test for title generation

--- src/tests/test_pdf_creator.py
+++ src/tests/test_pdf_creator.py
@@ -1,6 +1,23 @@
 # type: ignore
 
+import datetime
+
+import pytest
+
+from signer.pdf_creator import PdfCreator
+
 from . import utils
+
+
+@pytest.mark.parametrize("year", (2024, 2025))
+@pytest.mark.parametrize("month", (i + 1 for i in range(12)))
+def test_title(month: int, year: int):
+    name = "Name Surname"
+    result = utils.create_pdf_and_get_result(name=name, month=month, year=year)
+    date = datetime.datetime(year=year, month=month, day=1)
+    expected_title = PdfCreator.TITLE.format(date.strftime("%Y %B"))
+    assert result.name == name
+    assert result.title == expected_title
 
 
 def test_name():
