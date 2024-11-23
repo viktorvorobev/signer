447914a14a52dff8edb5807c78ce3956dfb78840
Viktor Vorobev <vikeyer@gmail.com>
2024-11-23 12:04:23 +0100

    src: tests: add tests for vacation and public holidays generation

--- src/tests/test_pdf_creator.py
+++ src/tests/test_pdf_creator.py
@@ -8,9 +8,12 @@ from signer.pdf_creator import PdfCreator
 
 from . import utils
 
+YEARS = (2024, 2025)
+MONTHS = tuple(range(1, 13))
 
-@pytest.mark.parametrize("year", (2024, 2025))
-@pytest.mark.parametrize("month", (i + 1 for i in range(12)))
+
+@pytest.mark.parametrize("year", YEARS)
+@pytest.mark.parametrize("month", MONTHS)
 def test_title(month: int, year: int):
     name = "Name Surname"
     result = utils.create_pdf_and_get_result(name=name, month=month, year=year)
@@ -24,3 +27,37 @@ def test_name():
     name = "TEST NAME"
     data = utils.create_pdf_and_get_result(name=name)
     assert data.name == name
+
+
+@pytest.mark.parametrize("year", YEARS)
+@pytest.mark.parametrize("month", MONTHS)
+def test_holiday(month: int, year: int):
+    name = "Name Surname"
+
+    public_holidays = utils.get_random_weekdays_in_month(year=year, month=month, days_amount=4)
+
+    result = utils.create_pdf_and_get_result(
+        name=name,
+        month=month,
+        year=year,
+        public_holidays=list(public_holidays),
+    )
+
+    assert result.public_holidays == len(public_holidays)
+
+
+@pytest.mark.parametrize("year", YEARS)
+@pytest.mark.parametrize("month", MONTHS)
+def test_vacation_days(month: int, year: int):
+    name = "Name Surname"
+
+    vacation_days = utils.get_random_weekdays_in_month(year=year, month=month, days_amount=5)
+
+    result = utils.create_pdf_and_get_result(
+        name=name,
+        month=month,
+        year=year,
+        vacation_days=list(vacation_days),
+    )
+
+    assert result.vacation_days == len(vacation_days)
